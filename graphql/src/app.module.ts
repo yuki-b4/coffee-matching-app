9061d88c8dcb2242a20eb7a6950a7556b4d83905
櫻田亮太 <tns.r.14xx@gmail.com>
2022-07-15 00:08:45 +0900

    graphql 環境構築

--- graphql/src/app.module.ts
+++ graphql/src/app.module.ts
@@ -1,10 +1,20 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: true,
+      playground: true,
+      autoSchemaFile: './src/schema.graphql',
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- setup.sh
+++ setup.sh
@@ -1,9 +1,17 @@
-# 作成中
+# /bin/bash
 
+# 作成中
+if [ $# -eq 0 ]
+    echo "Argument error"
+    echo "Pass the password"
+fi
 PASSWORD=$1
 touch .env
+touch ./graphql/.env
 echo "DB_PASSWOR = ${PASSWORD}" > .env
+echo "DATABASE_URL=\"mysql://root:${PASSWORD}@db:3306/coffee\"" > ./graphql/.env
 docker-compose build
 docker-compose run front yarn
 docker-compose run graphql yarn
+docker-compsoe down
 docker-compose up -d
